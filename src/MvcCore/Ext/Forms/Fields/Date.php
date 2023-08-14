3754a48f9c5398a23b13ceee4afc36de272951cb
Tom Flídr <tomflidr@gmail.com>
2023-08-15 01:52:07 +0200

    release v5.2.3

--- src/MvcCore/Ext/Forms/Fields/Date.php
+++ src/MvcCore/Ext/Forms/Fields/Date.php
@@ -44,7 +44,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.2';
+	const VERSION = '5.2.3';
 
 	/**
 	 * String format mask to format given values in `\DateTimeInterface` type for PHP `date_format()` function or 
